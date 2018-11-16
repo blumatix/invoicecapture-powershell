12509af270fbf52a19735940d63c6c7734b0f755
Rudolf Dittrich <rudi.dittrich77@gmail.com>
2018-11-16 09:11:31 +0100

    Added support for processing invoices in a given folder

--- README.md
+++ README.md
@@ -4,13 +4,20 @@ Contains powershell scripts to access our capture client
 __NOTE__: In order to being able to execute this script you may have to update the user preference for the PowerShell execution policy.
 For further information please refer to [powershell execution policy settings](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-6)
 
-
-## Usage:
+## Usage Examples
+The following example processes a single invoice.
 ```sh
 .\invoice_details.ps1 -filename PATH_TO_INVOICE_FILE -apiKey YOUR_API_KEY -version CAPTURE_VERSION -url CAPTURE_SDK_URL -resultPdf -csv
 ```
+
+This example processes all invoices within a certain folder. Only file with pdf, tiff, tif, jpeg, jpg or png extensions
+will be taken into account.
+```sh
+.\invoice_details.ps1 -folderPath PATH_TO_INVOICE_Folder -apiKey YOUR_API_KEY -version CAPTURE_VERSION -url CAPTURE_SDK_URL -resultPdf -csv
+```
+
 ## Result
-This script produces the following files
+This script produces the following files for each invoice
 - invoiceResult.csv: The InvoiceDetails predictions in csv format
 - invoiceResult.json: The prediction result in json format
 - invoiceResult.pdf: An invoice result pdf which contains markers for all detected invoice details
--- invoice_details.ps1
+++ invoice_details.ps1
@@ -1,12 +1,15 @@
 <#
     .Synopsis
-    Requests the InvoiceDetails prediction for a single invoice
+    Requests InvoiceDetails for a single invoice
 
     .Description
     This script can be used to request InvoiceDetails for a given invoice. The response of an
     InvoiceDetail request is written in json format to an invoiceResult.json file. Moreover
     it is also able to write a result invoice pdf file with markers for each detected InvoiceDetail
     as well as a csv file containing the InvoiceDetail prediction results.
+
+    .Parameter folderPath
+    A path to a directory which contains invoice to be processed
 
     .Parameter filename
     The path to the invoice file that shall be processed. Currently we support the following formats:
@@ -29,11 +32,11 @@
 
     .Parameter csv
     If set then a csv file is generated
-
 #>
 [CmdletBinding()]
 param (
-    [string]$filename,
+    [string]$folderPath="",
+    [string]$filename="",
     [string]$apiKey,
     [string]$url,
     [string]$version,
@@ -43,42 +46,49 @@ param (
 
 function WriteJson {
     param (
+        [string]$baseFilename,
         $resultObject
     )
 
     $result = $resultObject | ConvertTo-Json -Depth 10
-    $jsonFile = Join-Path -Path $currentLocation -ChildPath "invoiceResult.json"
+    $jsonFile = Join-Path -Path $currentLocation -ChildPath "$($baseFilename).json"
     write-host "Write InvoiceDetails predictions to $jsonFile"
     [System.IO.File]::WriteAllText($jsonFile, $result)
 }
 
 function WritePdf {
     param(
+        [string]$baseFilename,
         [string]$resultPdfBase64
     )
 
     $resultPdfInvoice = [Convert]::FromBase64String($resultPdfBase64)
-    $pdfFile = Join-Path -Path $currentLocation -ChildPath "invoiceResult.pdf"
-    write-host "Write InvoiceResultPdf to $pdfFile"
+    $pdfFile = Join-Path -Path $currentLocation -ChildPath "$($baseFilename).pdf"    
+    write-host "Write InvoiceResultPdf to $pdfFile"    
     [System.IO.File]::WriteAllBytes($pdfFile, $resultPdfInvoice)
 }
 
 function WriteCsv {
     param(
+        [string]$baseFilename,
         $predictionResult
     )
 
-    $csvFile = Join-Path -Path $currentLocation -ChildPath "invoiceResult.csv"
+    $csvFile = Join-Path -Path $currentLocation -ChildPath "$($baseFilename).csv"
+    
     write-host "Write InvoiceResult csv to $csvFile"
-    $singlePredictions = $predictionResult   | Select-Object -expand InvoiceDetailTypePredictions  `
-                                                | ConvertTo-Csv -NoTypeInformation -Delimiter "`t" `
-                                                | ForEach-Object {$_.Replace('"','')}
+
+    $singlePredictions = $predictionResult `
+        | Select-Object -expand InvoiceDetailTypePredictions  `
+        | ConvertTo-Csv -NoTypeInformation -Delimiter "`t" `
+        | ForEach-Object {$_.Replace('"','')}
 
     # VatGroups: VatRate, NetAmount, VatAmount 
-    $predictionGroups = ($predictionResult | Select-Object -expand PredictionGroups) `
-                                | Select-Object -expand InvoiceDetailTypePredictions `
-                                | ConvertTo-Csv -NoTypeInformation -Delimiter "`t" `
-                                | ForEach-Object {$_.Replace('"','')}
+    $predictionGroups = ($predictionResult `
+        | Select-Object -expand PredictionGroups) `
+        | Select-Object -expand InvoiceDetailTypePredictions `
+        | ConvertTo-Csv -NoTypeInformation -Delimiter "`t" `
+        | ForEach-Object {$_.Replace('"','')}
 
     # Merge predictions, skip header of prediction groups                                
     $csvResult = $singlePredictions + ($predictionGroups | Select-Object -skip 1)
@@ -86,41 +96,72 @@ function WriteCsv {
     $csvResult | Set-Content $csvFile
 }
 
+function PostRequest {
+    param (
+        $invoice,
+        $version
+    )
+
+    # Prepare request
+    $request = @{
+        "Filter" = 0;
+        "Invoice" = [Convert]::ToBase64String($invoice);
+        "Version" = $version;
+        "CreateResultPdf" = if ($resultPdf.IsPresent) {1} else {0};
+    } | ConvertTo-Json
+    
+    # Send request
+    $response = Invoke-WebRequest `
+                -Uri "$url/invoicedetail/detect" `
+                -Method POST `
+                -Body $request `
+                -ContentType "application/json" `
+                -Headers @{"accept"="application/json"; "X-ApiKey"= $apiKey}
+    
+    return $response
+}
+
+function ProcessInvoice {
+    param (
+        [string]$filename
+    )
+
+    $invoice=[System.IO.File]::ReadAllBytes($filename)
+
+    $response = PostRequest $invoice $version
+    
+    # On success            
+    if ($response.statuscode -eq 200) {
+        $baseFileName = (Get-ChildItem $filename).BaseName
+        $resultObject = $response.Content | ConvertFrom-Json
+    
+        WriteJson $baseFileName $resultObject
+        
+        if ($resultPdf -eq $true) {
+            WritePdf $baseFileName $resultObject.ResultPdf
+        }
+    
+        if ($csv -eq $true)
+        {
+            WriteCsv $baseFileName $resultObject
+        }
+    }
+    else {
+        Write-Host "Http status code $($response.statuscode), status descripion $($response.statusdescription)"
+    }    
+}
+
+# Results are written into the current folder
 Set-Location -Path "."
 $currentLocation = Get-Location
-$invoice=[System.IO.File]::ReadAllBytes($filename)
 
-# Prepare request
-$request = @{
-    "Filter" = 0;
-    "Invoice" = [Convert]::ToBase64String($invoice);
-    "Version" = $version;
-    "CreateResultPdf" = if ($resultPdf.IsPresent) {1} else {0};
-} | ConvertTo-Json
-
-# Send request
-$response = Invoke-WebRequest `
-            -Uri "$url/invoicedetail/detect" `
-            -Method POST `
-            -Body $request `
-            -ContentType "application/json" `
-            -Headers @{"accept"="application/json"; "X-ApiKey"= $apiKey}
-
-# On success            
-if ($response.statuscode -eq 200) {
-    $resultObject = $response.Content | ConvertFrom-Json
-
-    WriteJson $resultObject
-    
-    if ($resultPdf -eq $true) {
-        WritePdf $resultObject.ResultPdf
-    }
-
-    if ($csv -eq $true)
-    {
-        WriteCsv $resultObject
+if (-Not [string]::IsNullOrEmpty($folderPath) -and (Test-Path $folderPath)) {
+    $files = Get-ChildItem $folderPath | Where-Object {$_.extension -match 'pdf|tiff|tif|png|jpeg|jpg'} | ForEach-Object {$_.FullName}
+    foreach ($filename in $files) {        
+        write-host $filename
+        ProcessInvoice -filename $filename
     }
 }
-else {
-    Write-Host "Http status code $($response.statuscode), status descripion $($response.statusdescription)"
+elseif (Test-Path $filename) {
+    ProcessInvoice -filename $filename
 }
